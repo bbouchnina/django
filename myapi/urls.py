1dcd8803c7075b063038a4fe0b0b19b033effb78
Brahim BOUCHNINA <brahim.bouchnina@avaliance.com>
2021-01-19 14:34:55 +0100

    update

--- myapi/urls.py
+++ myapi/urls.py
@@ -12,10 +12,22 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/produits/<pk>', views.ProduitViewSet.as_view({'get': 'retrieve'}),
+    #     name='produits-detail'),
     url(
         r'^api/clients/$',
         views.ClientViewSet.as_view({'get': 'list', 'post': 'create'}),
         name='clients-list',
+    ),
+    url(
+        r'^api/produits/$',
+        views.ProduitViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='produits-list',
+    ),
+    url(
+        r'api/produits/(?P<pk>\d+)$',
+        views.ProduitViewSet.as_view({'get': 'retrieve', 'put':'update'}),
+        name='produits-detail',
     ),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
--- myapi/views.py
+++ myapi/views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets , status
 
-from .serializers import ClientSerializer
-from .models import Client
+from .serializers import ClientSerializer , ProduitSerializer
+from .models import Client , Produit
 from rest_framework.response import Response
-
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 
 class ClientViewSet(viewsets.ModelViewSet):
@@ -17,6 +16,34 @@ class ClientViewSet(viewsets.ModelViewSet):
 
     def create(self,request):
         serializer_class = ClientSerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProduitViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    def list(self,request):
+        queryset = Produit.objects.all().order_by('libelle')
+        serializer_class=ProduitSerializer(queryset,many=True)
+        return Response(serializer_class.data)
+
+    def retrieve(self,request,pk=None):
+        # queryset = Produit.objects.all()
+        produit = get_object_or_404(Produit,pk=pk)
+        serializer_class=ProduitSerializer(produit)
+        return Response(serializer_class.data)
+    def create(self,request):
+        serializer_class = ProduitSerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self,request,pk=None):
+        produit = get_object_or_404(Produit,pk=pk)
+        serializer_class=ProduitSerializer(produit,data=request.data)
         if serializer_class.is_valid():
             serializer_class.save()
             return Response(serializer_class.data, status=status.HTTP_201_CREATED)
